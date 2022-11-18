6eaaef408b37c197f7b007d3cf814b0ad445862f
Uğur Şölen <76258170+solen13@users.noreply.github.com>
2022-11-18 18:24:48 +0300

    18.11.22

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
   <v-app class="app"  >
     <nav-bar/>
-    <sepet v-if="this.$store.state.cardShows"/>
+    <shoppingBox v-if="this.$store.state.cardShows"/>
    <Nuxt />
   </v-app>
 </template>
 
 <script>
 import NavBar from "@/components/header/navBar";
-import Sepet from "@/components/Card/shopping-box";
+import shoppingBox from "@/components/Card/shopping-box";
 
 export default {
-  components: { Sepet, NavBar}
+  components: { shoppingBox, NavBar}
 }
 </script>
 <style>
